614778ada2797982efe6db13ff64c70005326dd7
Luke Hayes <lukepurplehayes@gmail.com>
2022-04-27 09:09:31 +0100

    Added MenuState.hx

---
+++ source/MenuState.hx
@@ -0,0 +1,20 @@
+package;
+
+import flixel.FlxG;
+import flixel.FlxState;
+import flixel.FlxSprite;
+
+// Front facing menu screen.
+class MenuState extends FlxState
+{
+
+    override public function create()
+    {
+        super.create();
+    }
+
+    override public function update(elapsed:Float)
+    {
+        super.update(elapsed);
+    }
+}
