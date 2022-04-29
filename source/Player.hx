b8c4ae33c3a275715b012ad0224dba08c02fb3a4
Luke Hayes <lukepurplehayes@gmail.com>
2022-04-29 17:31:52 +0100

    Updated Player.hx with better movement and drag.

--- source/Player.hx
+++ source/Player.hx
@@ -4,6 +4,7 @@ import flixel.FlxG;
 import flixel.FlxState;
 import flixel.FlxSprite;
 import flixel.util.FlxColor;
+import flixel.math.FlxPoint;
 import Input;
 
 // All player functionality lives inside here.
@@ -17,6 +18,7 @@ class Player extends FlxSprite
     {
         super(x,y);
         makeGraphic(16,16, FlxColor.BLUE);
+        drag.x = drag.y = 600;
     }
 
     public function setCameraFollow()
@@ -24,31 +26,59 @@ class Player extends FlxSprite
         FlxG.camera.follow(this, TOPDOWN, 5);
     }
 
+    public function updateMovement()
+    {
+        // Setup basic direction variables
+        var up    :Bool = false;
+        var down  :Bool = false;
+        var left  :Bool = false;
+        var right :Bool = false;
+
+        // Set the direction values to actual key presses.
+        up    = FlxG.keys.anyPressed([UP,    W]);
+        down  = FlxG.keys.anyPressed([DOWN,  S]);
+        left  = FlxG.keys.anyPressed([LEFT,  A]);
+        right = FlxG.keys.anyPressed([RIGHT, D]);
+
+        // Stop movement is two opposite directions are pressed.
+        if(up && down)
+            up = down = false;
+        if(left && right)
+            left = right = false;
+
+        if(up||down||left||right)
+        {
+            var newAngle:Float = 0;
+            if (up)
+            {
+                newAngle = -90;
+                if (left)
+                    newAngle -= 45;
+                else if (right)
+                    newAngle += 45;
+            }
+            else if (down)
+            {
+                newAngle = 90;
+                if (left)
+                    newAngle += 45;
+                else if (right)
+                    newAngle -= 45;
+            }
+            else if (left)
+                newAngle = 180;
+            else if (right)
+                newAngle = 0;
+
+            velocity.set(SPEED, 0);
+            velocity.rotate(FlxPoint.weak(0,0), newAngle);
+        }
+
+    }
+
     override public function update(elapsed:Float)
     {
+        updateMovement();
         super.update(elapsed);
-
-        if(Input.RIGHT())
-        {
-            this.xp += this.speed;
-        }
-
-        if(Input.LEFT())
-        {
-            this.xp -= this.speed;
-        }
-
-        if(Input.UP())
-        {
-            this.yp -= this.speed;
-        }
-
-        if(Input.DOWN())
-        {
-            this.yp += this.speed;
-        }
-
-        this.x = this.xp * this.speed * elapsed;
-        this.y = this.yp * this.speed * elapsed;
     }
 }
