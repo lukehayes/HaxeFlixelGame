ccb593cc0322bfe239b6de0f88a1e5cd8cd8af05
Luke Hayes <lukepurplehayes@gmail.com>
2022-05-07 12:06:49 +0100

    Basic Tilemap is now working.

--- source/tilemap/Tilemap.hx
+++ source/tilemap/Tilemap.hx
@@ -1,19 +1,27 @@
 package tilemap;
 
 import sys.io.File;
+import haxe.Json;
 import openfl.utils.Assets;
+import flixel.tile.FlxTilemap;
 
 /**
   Load and parse a Tiled tilemap from a JSON file.
-  */
+ */
 class Tilemap
 {
-    // Parsed JSON file
-    private var mapData : String;
+    public var mapData    :Dynamic;
+    public var background :FlxTilemap;
+    public var walls      :FlxTilemap;
 
-    public function new(jsonFile : String)
+    public function new(jsonFile : String, tilesetImage:String)
     {
-        // Load the JSON file into mapData
-        this.mapData = Assets.getText('assets/data/${jsonFile}');
+        this.mapData     = Json.parse(Assets.getText('assets/data/${jsonFile}'));
+        this.background = new FlxTilemap();
+        this.walls      = new FlxTilemap();
+
+        background.loadMapFromArray(mapData.layers[0].data, mapData.width, mapData.height, tilesetImage, 16,16, null, 1);
+        walls.loadMapFromArray(mapData.layers[1].data, mapData.width, mapData.height, tilesetImage, 16,16, null, 1);
     }
+
 }
