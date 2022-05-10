e958554720f93522859d0c299b427eba6177f992
Luke Hayes <lukepurplehayes@gmail.com>
2022-05-10 17:39:40 +0100

    Added TestTile map to PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -3,11 +3,13 @@ package;
 import flixel.FlxG;
 import flixel.FlxState;
 import Player;
+import tilemap.Tilemap;
 
 class PlayState extends FlxState
 {
     var text   : flixel.text.FlxText;
     var player : Player;
+    var map    : Tilemap;
 
     override public function create()
     {
@@ -16,6 +18,12 @@ class PlayState extends FlxState
         this.text = new flixel.text.FlxText(0, 0, 0, "Initalizing...", 64);
         this.text.screenCenter();
         add(this.text);
+
+        this.map = new Tilemap("TestTilemap.json", "TileImage.png");
+        this.map.background.screenCenter();
+        this.map.walls.screenCenter();
+        add(this.map.background);
+        add(this.map.walls);
 
         this.player = new Player();
         add(this.player);
@@ -26,6 +34,9 @@ class PlayState extends FlxState
     {
         super.update(elapsed);
         player.update(elapsed);
+
+        // Basic collision check for player on walls!
+        FlxG.collide(this.player, this.map.walls);
 
     }
 }
