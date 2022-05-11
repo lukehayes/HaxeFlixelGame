a42c5e362a85b93b1f72abe8c618b2143c901c70
lukehayes <lukepurplehayes@gmail.com>
2022-05-11 20:19:28 +0100

    Added FlxTween functionality to Coin class.

--- source/Coin.hx
+++ source/Coin.hx
@@ -2,6 +2,8 @@ package;
 
 import flixel.FlxSprite;
 import flixel.util.FlxColor;
+import flixel.tweens.FlxTween;
+import flixel.tweens.FlxEase;
 
 class Coin extends FlxSprite
 {
@@ -9,5 +11,20 @@ class Coin extends FlxSprite
     {
         super(x,y);
         makeGraphic(8,8, FlxColor.YELLOW);
+    }
+
+    override public function kill()
+    {
+        alive = false;
+        FlxTween.tween(this,
+                {alpha:0, y: y - 16},
+                0.33,
+                {ease: FlxEase.circOut, onComplete: this.finishKill}
+                );
+    }
+
+    public function finishKill(_)
+    {
+        exists = false;
     }
 }
